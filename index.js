5d521c941e4351134b5ddd322f8303fc96a0d574
Myles Reyburn <mylesreyburn@gmail.com>
2023-02-05 20:37:52 -0700

    tests won't pass in terminal for indeterminate reasons, attempting to test via codegrade

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Add your code here
+
+
+function submitData(userName, userEmail){
+    // let userObject = {
+    //     name: userName,
+    //     email: userEmail
+    // }
+    let userObject = {};
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    }
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then((response) => response.json())
+        .then((object) => {
+            document.append(object)
+        })
+        .catch((error) => {
+            alert("uh oh! someone's [ERROR]")
+            document.append(error.message)
+        })
+}
+
+submitData("John Jingle", "jj@hohoho.hotmail.biz")
+
+
+
+
+
+
+/* fetch("http://localhost:3000/dogs", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  }); */
+
+
+
+
+
+

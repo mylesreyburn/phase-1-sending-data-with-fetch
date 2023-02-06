11e66461a162058b0e2ea54a4829c8b39acf51a0
Myles Reyburn <mylesreyburn@gmail.com>
2023-02-05 21:23:47 -0700

    still failing one test in terminal, but that test seems redundant so we're gonna give it another shot

--- index.js
+++ index.js
@@ -1,38 +1,68 @@
 // Add your code here
 
 
-function submitData(userName, userEmail){
-    // let userObject = {
-    //     name: userName,
-    //     email: userEmail
-    // }
-    let userObject = {};
+/* function submitData(userName, userEmail){
+    let userObject = {
+        name: userName,
+        email: userEmail
+    }
     const configurationObject = {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json",
         },
-        body: JSON.stringify({
-            name: userName,
-            email: userEmail
-        })
+        body: JSON.stringify(userObject)
     }
     return fetch("http://localhost:3000/users", configurationObject)
-        .then((response) => response.json())
-        .then((object) => {
-            document.append(object)
+        .then(function(response){
+            let egg = response.json()
+            console.log("response.json:", egg)
+            return egg
+        })
+        .then(function(object){
+            console.log(object)
+            document.querySelector("body").append(object)
         })
         .catch((error) => {
             alert("uh oh! someone's [ERROR]")
-            document.append(error.message)
+            document.querySelector("body").append(error.message)
+        })
+}
+
+submitData("John Jingle", "jj@hohoho.hotmail.biz") */
+
+
+
+function submitData(userName, userEmail){
+    let userObject = {
+        name: userName,
+        email: userEmail
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userObject)
+    })
+        .then(function(response){
+            let egg = response.json()
+            console.log("response.json:", egg)
+            return egg
+        })
+        .then(function(object){
+            console.log(object)
+            document.querySelector("body").append(object)
+        })
+        .catch((error) => {
+            alert("uh oh! someone's [ERROR]")
+            document.querySelector("body").append(error.message)
         })
 }
 
 submitData("John Jingle", "jj@hohoho.hotmail.biz")
-
-
-
 
 
 
